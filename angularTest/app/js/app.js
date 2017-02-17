0df95c72976e57d1bb5aaa6b6cfa5babeb269691
Bradley Ouellette <bouellette@mixpo.com>
2017-02-17 13:15:24 -0800

    Lecture 12

--- angularTest/app/js/app.js
+++ angularTest/app/js/app.js
@@ -1,9 +1,12 @@
 var testingAngularApp = angular.module('testingAngularApp', []);
 
-testingAngularApp.controller('testingAngularCtrl', function($rootScope, $scope) {
+testingAngularApp.controller('testingAngularCtrl', function($rootScope, $scope, $http) {
    $scope.title = "Testing Angular JS Applications"; 
     
     $scope.destinations = [];
+    
+    $scope.apiKey = "1918c6dd5f6a30b480c11210c92db95f";
+    
     
     $scope.newDestination = {
         city: undefined,
@@ -21,5 +24,23 @@ testingAngularApp.controller('testingAngularCtrl', function($rootScope, $scope) 
     
     $scope.removeDestination = function (index) {
         $scope.destinations.splice(index, 1);
+    }
+    
+    $scope.getWeather = function(destination) {
+        $http.get("http://api.openweathermap.org/data/2.5/weather?q="+ destination.city + "&appid=" + $scope.apiKey).then(
+        function successCallback (response) {
+            if (response.data.weather) {
+                destination.weather = {};
+                destination.weather.main = response.data.weather[0].main;
+                destination.weather.temp = $scope.convertKelvinToCelsius(response.data.main.temp);
+            }
+        },
+        function errorCallback (error) {
+            console.log(error);
+        });
+    };
+    
+    $scope.convertKelvinToCelsius = function (temp) {
+        return Math.round(temp- 273);
     }
 });
--- angularTest/app/js/test/unit/testingAngularUnitSpec.js
+++ angularTest/app/js/test/unit/testingAngularUnitSpec.js
@@ -4,16 +4,19 @@ describe('Testing Angular JS Test Suite', function(){
     
     describe('Testing Angular JS Controller', function() {
        
-        var scope, ctrl;
+        var scope, ctrl, httpBackend;
         
         beforeEach(
-            inject(function($controller,$rootScope) {
+            inject(function($controller,$rootScope, $httpBackend) {
                 scope=$rootScope.$new();
                 ctrl = $controller('testingAngularCtrl', {$scope:scope});
+                httpBackend=$httpBackend;
             })
         );
         
         afterEach(function() {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
            // Cleanup code 
         });
         
@@ -70,6 +73,26 @@ describe('Testing Angular JS Test Suite', function(){
             expect(scope.destinations[0].city).toBe("Warsaw");
             expect(scope.destinations[0].country).toBe("Poland");
             
+            
+        });
+        
+        it('should update the weather for a specific destination', function() {
+            scope.destination = {
+                city: "Melbourne",
+                country: "Australia"
+            };
+            httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q="+ scope.destination.city + "&appid=" + scope.apiKey).respond(
+            {
+                weather: [{main: 'Rain', detail: 'Light rain'}],
+                main: {temp:288}
+            });
+            
+            scope.getWeather(scope.destination);
+            
+            httpBackend.flush();
+            
+            expect(scope.destination.weather.main).toBe("Rain");
+            expect(scope.destination.weather.temp).toBe(15);
             
         });
     });
