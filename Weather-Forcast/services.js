4cde492d3c2fb49391554eeeec93d2e62b46f122
Bradley Ouellette <bouellette@mixpo.com>
2017-03-17 09:53:08 -0700

    Angular - Bonus Complete

--- Weather-Forcast/services.js
+++ Weather-Forcast/services.js
@@ -4,3 +4,14 @@ weatherApp.service('cityService', function() {
     this.city = "New York, NY";
     
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+   
+    this.GetWeather = function(city, days) {
+        var appID = "1918c6dd5f6a30b480c11210c92db95f"
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=1918c6dd5f6a30b480c11210c92db95f", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+        return weatherResult = weatherAPI.get({ appid: appID, q: city, cnt: days});
+    };
+    
+}]);
