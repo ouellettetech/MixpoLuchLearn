6c5872809133ead2b473a70cbeeb52b33ead9ec9
Bradley Ouellette <bouellette@mixpo.com>
2017-03-19 00:44:02 -0700

    Node.js: Leason 20

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,2 +1,15 @@
-var greet = require('./greet');
-greet();
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+}
+
+Person.prototype.greet = function(){
+    console.log('Hello, '+ this.firstName + ' ' +
+    this.lastName);
+};
+
+var john = new Person('John', 'Doe');
+john.greet();
+var jane = new Person('Jane', 'Doe');
+jane.greet();
