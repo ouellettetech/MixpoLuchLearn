a8245f4cdf7880273b77fba0e20e8064d577179b
Bradley Ouellette <bouellette@mixpo.com>
2017-03-19 00:27:50 -0700

    Node.js: Leason 18

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,18 +1,2 @@
-// function stateent
-function greet() {
-    console.log('hi');
-}
+var greet = require('./greet');
 greet();
-
-// functions are first-clas
-function logGreeting(fn) {
-    fn();
-}
-logGreeting(greet);
-
-var greetMe = function() {
-    console.log('Hi Brad');
-}
-greetMe();
-
-logGreeting(greetMe);
