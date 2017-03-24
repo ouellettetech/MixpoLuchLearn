23d5c1790fef580bb6580a2bac00d268478c44d1
Bradley Ouellette <bouellette@mixpo.com>
2017-03-24 12:13:32 -0700

    Weird Parts: Leason 28

--- weirdParts/app.js
+++ weirdParts/app.js
@@ -1,16 +1,24 @@
-//Long Running Function
-function waitThreeSeconds(){
-    var ms = 3000 + new Date().getTime();
-    while(new Date() <ms ){}
-    console.log('finished function');
+////Long Running Function
+//function waitThreeSeconds(){
+//    var ms = 3000 + new Date().getTime();
+//    while(new Date() <ms ){}
+//    console.log('finished function');
+//}
+//
+//function clickHandler() {
+//    console.log('click event!');
+//}
+//
+//// listen for the click event
+//document.addEventListener('click', clickHandler);
+//
+//waitThreeSeconds();
+//console.log("finished execution");
+
+function greet(name){
+    name = name || '<Your name here>';
+    console.log('hello ' + name);
 }
 
-function clickHandler() {
-    console.log('click event!');
-}
-
-// listen for the click event
-document.addEventListener('click', clickHandler);
-
-waitThreeSeconds();
-console.log("finished execution");
+greet('Tony');
+greet();
