7e8e3d171f8fe1b6c50b12e155a04d56583f06e0
Bradley Ouellette <bouellette@mixpo.com>
2017-03-25 00:01:40 -0700

    Node.js: Leason 39

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,10 +1,23 @@
-var obj = {
-    name: 'John Doe',
-    greet: function() {
-        console.log(`Hello ${ this.name}`);
-    }
-}
+var EventEmitter = require('events');
+var util = require('util');
 
-obj.greet();
-obj.greet.call({ name: 'Jane Doe'});
-obj.greet.apply({ name: 'Jane Doe'});
+function Greetr() {
+    EventEmitter.call(this);
+    this.greeting = 'Hello World';
+};
+
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data){
+    console.log(this.greeting + ': ' + data);
+    this.emit('greet', data);
+};
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+    console.log('Someone greeted!: ' + data);
+});
+
+greeter1.greet('Brad');
