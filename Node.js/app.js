0f89ea768426609f40d9242ce2283bfd19f2adba
Bradley Ouellette <bouellette@mixpo.com>
2017-03-26 16:27:50 -0700

    Node.js: Leason 53

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
 var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 1*1024});
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
 
+var gzip = zlib.createGzip();
+readable.pipe(writable);
 
-readable.on('data',function(chunk) {
-    console.log(chunk);
-    writable.write(chunk);
-});
+readable.pipe(gzip).pipe(compressed);
