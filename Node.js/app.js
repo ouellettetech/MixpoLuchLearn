1428be5c6447d182f445f48d1e697f9f7628ff7b
Bradley Ouellette <bouellette@mixpo.com>
2017-03-26 15:51:57 -0700

    Node.js: Leason 51

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,13 +1,10 @@
 var fs = require('fs');
 
-var greet = fs.readFileSync(__dirname + '/greet.txt',
-'utf8');
-console.log(greet);
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 1*1024});
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
 
-var greet2 = fs.readFile(__dirname + '/greet.txt','utf8',
-function(err, data) {
-    console.log(data);
+readable.on('data',function(chunk) {
+    console.log(chunk);
+    writable.write(chunk);
 });
-
-console.log('Done!');
