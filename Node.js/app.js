576c42ff060a81c2e1361d6cbef07d6bca83ab6e
Bradley Ouellette <bouellette@mixpo.com>
2017-03-27 00:05:06 -0700

    Node.js: Leason 64

--- Node.js/app.js
+++ Node.js/app.js
@@ -2,6 +2,20 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-    res.writeHead(200,{ 'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-}).listen(1337, '127.0.0.1');
+    if(req.url === '/') {
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    else if(req.url === '/api'){
+        res.writeHead(200,{ 'Content-Type': 'application/json'});
+
+        var obj = {
+            firstName: 'John',
+            lastName: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
+}).listen(1337, '0.0.0.0');
