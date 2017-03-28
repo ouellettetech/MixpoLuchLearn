0a1ec42e1616dd2bc12ce8feb6ab3340567b7769
Bradley Ouellette <bouellette@mixpo.com>
2017-03-27 23:13:55 -0700

    Node.js: Leason 75

--- Node.js/app.js
+++ Node.js/app.js
@@ -4,7 +4,14 @@ var app = express();
 var port = process.env.PORT || 1337
 
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
+});
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function(req,res,next){
+    console.log('Request URL: ' + req.url);
+    next();
 });
 
 app.get('/person/:id', function(req, res) {
