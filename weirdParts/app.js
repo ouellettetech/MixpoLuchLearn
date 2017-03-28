c32d3610197cc7df4d129abf7accb35ea82a2b32
Bradley Ouellette <bouellette@mixpo.com>
2017-03-27 17:01:38 -0700

    Weird Parts: Leason 37

--- weirdParts/app.js
+++ weirdParts/app.js
@@ -22,17 +22,43 @@
 //
 //greet('Tony');
 //greet();
+//
+//var person = new Object();
+//
+////computed member access operator
+//person['firstname'] = "Tony";
+//person['lastname'] = "Alicea";
+//
+//var firstNameProperty = "firstname";
+//
+//console.log(person);
+//console.log(person[firstNameProperty]);
+//
+//// MemberAccess
+//console.log(person.firstname);
+//
+//function greet() {
+//    console.log('hi');
+//}
+//
+//greet.language = 'english';
+//console.log(greet.language);
 
-var person = new Object();
 
-//computed member access operator
-person['firstname'] = "Tony";
-person['lastname'] = "Alicea";
+var c = {
+    name: 'the c object',
+    log: function() {
+        var self = this;
+        self.name ='Updated c obect';
+        console.log(self);
+        
+        var setname = function(newname) {
+            self.name = newname;
+        }
+        
+        setname('Updated again! the c object');
+        console.log(self)
+    }
+}
 
-var firstNameProperty = "firstname";
-
-console.log(person);
-console.log(person[firstNameProperty]);
-
-// MemberAccess
-console.log(person.firstname);
+c.log();
