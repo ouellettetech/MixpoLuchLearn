14da5534d9d714359323e4d53c7d2be5243cca5d
Bradley Ouellette <bouellette@mixpo.com>
2017-03-28 23:43:21 -0700

    Node.js: Leason 83

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,5 +1,47 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+var waitTill = new Date(new Date().getTime() + 10 * 1000);
+while(waitTill > new Date()){}
+
+
+mongoose.connect('mongodb://db:27017/addressbook');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+    firstname: 'John',
+    lastname: 'Dow',
+    address: '555 Main St.'
+});
+
+john.save(function(err){
+    if (err) throw err;
+
+    console.log('person saved!');
+});
+
+var jane = Person({
+    firstname: 'Jane`',
+    lastname: 'Dow',
+    address: '555 Main St.'
+});
+
+
+jane.save(function(err){
+    if (err) throw err;
+
+    console.log('person saved!');
+});
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,6 +54,15 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req,res,next){
     console.log('Request URL: ' + req.url);
+
+    // get all the users
+    Person.find({}, function(err, users) {
+        if(err) throw err;
+
+        // object of all the users
+        console.log(users);
+    });
+
     next();
 });
 
