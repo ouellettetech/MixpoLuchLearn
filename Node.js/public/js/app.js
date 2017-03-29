4598ca121de49d667d1ee5b7bc36d26c6744a844
Bradley Ouellette <bouellette@mixpo.com>
2017-03-29 10:35:22 -0700

    Node.js: Leason 88

---
+++ Node.js/public/js/app.js
@@ -0,0 +1,18 @@
+angular.module('TestApp',[]);
+
+angular.module('TestApp').controller('MainController', ctrlFunc);
+
+function ctrlFunc() {
+    this.message = 'Hello';
+
+    this.people = [
+        {
+            name: 'John Doe'
+        },
+        {
+            name: 'Jane Doe'
+        },
+        {
+            name: 'Jim Doe'
+        }];
+}
