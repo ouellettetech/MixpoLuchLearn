baef6666eee59c7aea61d7f388bddb7a805c8f7e
Bradley Ouellette <bouellette@mixpo.com>
2017-03-30 09:24:31 -0700

    Node.js: Leason 95

--- Node.js/app.js
+++ Node.js/app.js
@@ -3,10 +3,15 @@ var app = express();
 var config = require('./config');
 var port = process.env.PORT || 1337;
 var mongoose = require('mongoose');
+var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
+
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
+setupController(app);
+apiController(app);
 
 app.listen(port);
