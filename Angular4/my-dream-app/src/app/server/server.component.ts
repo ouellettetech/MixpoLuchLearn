9a05a7b1dfc0e78db2307f7a284f8c6efbdd60da
Bradley Ouellette <bouellette@mixpo.com>
2017-04-24 22:20:08 -0700

    Angular4: Leason 36

--- Angular4/my-dream-app/src/app/server/server.component.ts
+++ Angular4/my-dream-app/src/app/server/server.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponet {
     serverId: number = 10;
     serverStatus: string = 'offline';
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red'; 
+    }
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
