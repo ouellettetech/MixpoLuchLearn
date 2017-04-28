c7f361ba035b614129a30b38c8efdb1eda98cd01
Bradley Ouellette <bouellette@mixpo.com>
2017-04-27 19:26:25 -0700

    Angular4: Leason 47

--- Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe('A test recipe', 'this is a test recipe',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png');
+  ];
   constructor() { }
 
   ngOnInit() {
