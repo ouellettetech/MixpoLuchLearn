4c5a1783e5ff3af105ec82750606c69faf0c76dc
Bradley Ouellette <bouellette@mixpo.com>
2017-04-30 13:29:59 -0700

    Angular4: Leason 72

--- Angular4/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular4/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +18,52 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None or Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy{
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name: string;
 
-  ngOnInit() {
+  constructor() { 
+    console.log('Constructor Called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('On Changes Called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('Init Called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck Called');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called');
+  }
+  
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+  }
+  
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
+  }
 }
