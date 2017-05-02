d32dfe3863e0ae605eb09e4e1141d77902113eb9
Bradley Ouellette <bouellette@mixpo.com>
2017-05-02 09:14:16 -0700

    Angular4: Leason 79

--- Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('A test recipe', 'this is a test recipe',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png'),
-    new Recipe('A test recipe', 'this is a test recipe',
+    new Recipe('Another test recipe', 'this is a test recipe',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png')
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe)
+  {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
