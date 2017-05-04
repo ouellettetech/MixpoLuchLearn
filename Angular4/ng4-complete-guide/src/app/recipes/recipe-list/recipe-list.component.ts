540415964bf104351a56da639481a328d3378957
Bradley Ouellette <bouellette@mixpo.com>
2017-05-04 12:31:16 -0700

    Angular4: Leason 105

--- Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes : Recipe[] = [
-    new Recipe('A test recipe', 'this is a test recipe',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png'),
-    new Recipe('Another test recipe', 'this is a test recipe',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png')
-  ];
-  constructor() { }
+  recipes : Recipe[];
+
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe: Recipe)
   {
---
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    private recipes : Recipe[] = [
+        new Recipe('A test recipe', 'this is a test recipe',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png'),
+        new Recipe('Another test recipe', 'this is a test recipe',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
