5f291d26a9d955a9d09a6d4e95cd05dc4453884f
Bradley Ouellette <bouellette@mixpo.com>
2017-05-04 17:30:02 -0700

    Angular4: Leason 109

--- Angular4/ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,14 +1,27 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'this is a test recipe',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png'),
-        new Recipe('Another test recipe', 'this is a test recipe',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty Scnutzel - just awsome',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Meat', 2),
+            ]),
+        new Recipe(
+            'Big Far Burger',
+            'this is a test recipe',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/2000px-Recipe-575434.svg.png',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+            ])
     ];
 
     getRecipes() {
