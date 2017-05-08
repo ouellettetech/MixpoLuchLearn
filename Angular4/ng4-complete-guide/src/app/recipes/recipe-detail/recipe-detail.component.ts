c2d054f34d3c9533dc79e5bec4aa3fed273c492f
Bradley Ouellette <bouellette@mixpo.com>
2017-05-07 18:42:42 -0700

    Angular4: Leason 154

--- Angular4/ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Subscription } from 'rxjs/Subscription';
@@ -13,8 +13,9 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
   paramsSubscription: Subscription;
-  constructor(private recipeService: RecipeService, 
-              private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.route.params
@@ -30,4 +31,8 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
 }
