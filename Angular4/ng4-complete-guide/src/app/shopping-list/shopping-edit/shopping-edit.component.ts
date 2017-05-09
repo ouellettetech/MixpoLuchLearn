89fbba8934510ea1df1f898aa09f8bd004dce073
Bradley Ouellette <bouellette@mixpo.com>
2017-05-09 12:20:32 -0700

    Angular4: Leason 201

--- Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared//ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,17 +9,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredent = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm ) {
+    const value = form.value;
+    const newIngredent = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredent);
   }
 
