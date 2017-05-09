6d16d61b70c2bb709be4d84a4207e321ffb0731a
Bradley Ouellette <bouellette@mixpo.com>
2017-05-08 22:43:44 -0700

    Angular4: Assignment 6-mine

--- Angular4/forms-td-assignment/src/app/app.component.ts
+++ Angular4/forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    @ViewChild('f') signupForm: NgForm;
+    defaultType = 'pro';
+    submitted = false;
 
+    user = {
+      email : '',
+      type : '',
+      password : ''
+    };
+
+    onSubmit() {
+      console.log(this.signupForm);
+      this.submitted = true;
+      this.user.email = this.signupForm.value.email;
+      this.user.type = this.signupForm.value.type;
+      this.user.password = this.signupForm.value.password;
+
+      this.signupForm.reset();
+  }
 }
