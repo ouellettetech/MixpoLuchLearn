0b65c8d9b13c5a71a3b80d9fd9d7ff6c0c5967fc
Bradley Ouellette <bouellette@mixpo.com>
2017-05-09 17:25:31 -0700

    Angular4: Leason 203

--- Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared//ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onAddItem(form: NgForm ) {
