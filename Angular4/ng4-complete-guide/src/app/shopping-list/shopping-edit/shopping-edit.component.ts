7b5c7da91375226414edcfbc09bbcc45c2eb6995
Bradley Ouellette <bouellette@mixpo.com>
2017-05-09 17:41:08 -0700

    Angular4: Leason 206

--- Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular4/ng4-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,7 +34,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm ) {
+  onSubmit(form: NgForm ) {
     const value = form.value;
     const newIngredent = new Ingredient(value.name, value.amount);
     if(this.editMode){
@@ -42,6 +42,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredent);
     }
+    form.reset();
+    this.editMode = false;
   }
 
 }
