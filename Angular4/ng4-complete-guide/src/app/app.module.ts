a12d6d95054226b776e22e92b2c2c97e4ba0848e
Bradley Ouellette <bouellette@mixpo.com>
2017-05-12 12:39:20 -0700

    Angular4: Leason 211

--- Angular4/ng4-complete-guide/src/app/app.module.ts
+++ Angular4/ng4-complete-guide/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -34,7 +34,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule, 
+    ReactiveFormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
