615f9ba11412e846b4dc28942accb746e363a77a
Bradley Ouellette <bouellette@mixpo.com>
2017-05-12 17:54:28 -0700

    Angular4: Leason 216

--- Angular4/ng4-complete-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Angular4/ng4-complete-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService) { }
+              private recipeService: RecipeService,
+              private router: Router) { }
 
 
   ngOnInit() {
@@ -41,6 +42,11 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['..'], {relativeTo: this.route});
   }
 
   onAddIngredient() {
