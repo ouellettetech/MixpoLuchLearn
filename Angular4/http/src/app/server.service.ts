818dde09d6efaaf2a2061eb2fdce99140824b06d
Bradley Ouellette <bouellette@mixpo.com>
2017-05-18 19:31:10 -0700

    Angular4: Leason 236

--- Angular4/http/src/app/server.service.ts
+++ Angular4/http/src/app/server.service.ts
@@ -6,7 +6,10 @@ export class ServerSerice {
     constructor(private http: Http) {}
     storeServers(servers: any[]) {
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('https://udemy-ng-http-be9e7.firebaseio.com/data.json',
+        // return this.http.post('https://udemy-ng-http-be9e7.firebaseio.com/data.json',
+        //     servers,
+        //     {headers: headers});
+        return this.http.put('https://udemy-ng-http-be9e7.firebaseio.com/data.json',
             servers,
             {headers: headers});
     }
