feda191620c9ea18a0a5d9e40eeedbbadd8f9511
Bradley Ouellette <bouellette@mixpo.com>
2017-05-18 19:39:45 -0700

    Angular4: Leason 238

--- Angular4/http/src/app/app.component.ts
+++ Angular4/http/src/app/app.component.ts
@@ -39,9 +39,8 @@ export class AppComponent {
   }
   onGet() {
     this.serverService.getServers()
-      .subscribe((servers: any[]) => {
-        console.log(servers);
-      }, (error) => console.log(error)
+      .subscribe((servers: any[]) => this.servers = servers,
+      (error) => console.log(error)
       );
   }
   private generateId() {
