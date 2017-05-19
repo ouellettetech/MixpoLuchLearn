ce295dc792586e26f18a24a9bed0f33b96b810fe
Bradley Ouellette <bouellette@mixpo.com>
2017-05-19 09:13:10 -0700

    Angular4: Leason 245

--- Angular4/ng4-complete-guide/src/app/shared/data-storage.service.ts
+++ Angular4/ng4-complete-guide/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -13,11 +14,21 @@ export class DataStorageService {
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-90414.firebaseio.com/recipes.json')
-            .subscribe(
-                (response: Response) => {
-                    const recipes = response.json();
-                    this.recipeService.setRecipes(recipes);
-                });
+        this.http.get('https://ng-recipe-book-90414.firebaseio.com/recipes.json').map(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                for (const recipe of recipes) {
+                    if (!recipe['ingredients']) {
+                        console.log(recipe);
+                        recipe['ingredients'] = [];
+                    }
+                }
+                return recipes;
+            }
+        )
+        .subscribe(
+            (recipes: Recipe[]) => {
+                this.recipeService.setRecipes(recipes);
+            });
     }
 }
