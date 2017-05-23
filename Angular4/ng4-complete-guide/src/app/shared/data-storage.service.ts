e2e788bc1c3ff7118d9267114de6ec20d1a9dbb2
Bradley Ouellette <bouellette@mixpo.com>
2017-05-22 17:50:50 -0700

    Angular4: Leason 254

--- Angular4/ng4-complete-guide/src/app/shared/data-storage.service.ts
+++ Angular4/ng4-complete-guide/src/app/shared/data-storage.service.ts
@@ -3,18 +3,25 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(
+        private http: Http,
+        private recipeService: RecipeService,
+        private authService: AuthService) {}
 
      storeRecipes() {
-        return this.http.put('https://ng-recipe-book-90414.firebaseio.com/recipes.json',
+         const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-90414.firebaseio.com/recipes.json?auth=' + token,
             this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-90414.firebaseio.com/recipes.json').map(
+        const token = this.authService.getToken();
+
+        this.http.get('https://ng-recipe-book-90414.firebaseio.com/recipes.json?auth=' + token).map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
                 for (const recipe of recipes) {
