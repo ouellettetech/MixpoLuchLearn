d153469c5eefa20eb70a7ee0517bfeb402a358bc
Bradley Ouellette <bouellette@mixpo.com>
2017-05-24 18:20:11 -0700

    Angular4: Leason 269

--- Angular4/ng4-complete-guide/src/app/app-routing.module.ts
+++ Angular4/ng4-complete-guide/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
