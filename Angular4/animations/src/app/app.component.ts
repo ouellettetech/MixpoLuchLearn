82ba224a6314484717d1dc9775423b5074f0898d
Bradley Ouellette <bouellette@mixpo.com>
2017-05-25 23:55:21 -0700

    Angular4: Leason 294

--- Angular4/animations/src/app/app.component.ts
+++ Angular4/animations/src/app/app.component.ts
@@ -43,6 +43,27 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 				animate(500)
 			])
 		]),
+		trigger('list1', [
+			state('in', style({
+				'background-color': 'red',
+				opacity: 1,
+				transform: 'translateX(0)'
+			})),
+			transition('void => *', [
+				style({
+					opacity: 0,
+					transform: 'translatex(-100px)'
+				}),
+				animate(300),
+			]),
+			transition('* => void', [
+				animate(300,
+				style({
+					opacity: 0,
+					transform: 'translatex(100px)'
+				}))
+			]),
+		]),
 	]
 })
 export class AppComponent {
