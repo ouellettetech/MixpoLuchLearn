d7d2f20f8a51ff8013fd7cb30288e8a75b4da70f
Bradley Ouellette <bouellette@mixpo.com>
2017-05-29 22:32:44 -0700

    Angular4: Leason 305

--- Angular4/Testing/src/app/user/user.component.spec.ts
+++ Angular4/Testing/src/app/user/user.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
+
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -41,6 +43,35 @@ describe('UserComponent', () => {
     expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
   });
 
+  it('shouldn\'t display the user name if not logged in.', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
+  });
+
+  it('shouldn\'t fetch data successfully if not called asyncly.', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);
+  });
+
+  it('shouldn\'t fetch data successfully if called asyncly.', async(() => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('data'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe('data');
+    })
+  }));
   // let component: UserComponent;
   // let fixture: ComponentFixture<UserComponent>;
 
