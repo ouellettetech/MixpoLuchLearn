7f095957d317235869e0ee9bfccc5b2d94316395
Bradley Ouellette <bouellette@mixpo.com>
2017-05-30 23:19:22 -0700

    Angular4: Leason 315

---
+++ Angular4/Custom/src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css'],
+})
+
+
+export class UsersComponent {
+
+}
